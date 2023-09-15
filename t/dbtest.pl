82bc784df26124c52fa95dd27796db91c9cc3159
Domino-Z <zhangh0304@yeah.net>
2023-09-15 22:00:06 +0800

    deleted:    t/dbtest.pl

--- t/dbtest.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-use DBI;
-my $db_host = 'localhost';
-my $db_user = 'program';
-my $db_pass = 'program';
-my $db_name = 'VMStorageControl';
-my $db = "dbi:pg:dbname=${db_name};host=${db_host}";
-$dbh = DBI->connect($db, $db_user, $db_pass,{ RaiseError => 1, AutoCommit => 0 }) 
-
