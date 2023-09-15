11cbafab909d19561bb0554d6d10f406020fc5ce
Domino-Z <zhangh0304@yeah.net>
2023-09-15 14:20:08 +0800

    modified:   lib/VirtualMachine.pm

--- lib/VirtualMachine.pm
+++ lib/VirtualMachine.pm
@@ -14,19 +14,35 @@ sub new {
 }
 
 sub create {
-
+    my ($self, $name, $os, $storage_id) = @_;
+    my $sql = "INSERT INTO virtual_machine (name, os, storage_id) VALUES(?, ?, ?)";
+    my $sth = $self->{dbh}->prepare($sql);
+    $sth->execute($name, $os, $storage_id);
+    return $self->{dbh}->last_insert_id()
 }
 
 sub read {
-    my 
+    my ($self, $id) = @_;
+    my $sql = "SELECT * FROM virtual_machine WHERE id = ?";
+    my $sth = $self->{dbh}->prepare($sql);
+    $sth->execute($id);
+    return $self->fetchrow_hashref();
 }
 
 sub update {
-
+    my ($self, $id, $name, $os, $storage_id) = @_;
+    my $sql = "UPDATE virtual_machine SET name = ?, os = ?,storage_id = ? WHERE id = ?";
+    my $sth = $self->{dbh}->prepare($sql);
+    $sth->execute($name, $os, $storage_id, $id);
+    return 1;
 }
 
 sub delete {
-    
+    my ($self, $id) = @_;
+    my $sql = "DELETE FROM virtual_machine WHERE id =?";
+    my $sth = $self->{dbh}->prepare($sql);
+    $sth->execute($id);
+    return 1;
 }
 
 1;
